e8f0d3f8fead0e91f4776861430041277c9ac79b
selim-haspolat <selimhaspolat@gmail.com>
2024-03-25 15:09:34 +0300

    prisma fix

--- prisma/prismadb.js
+++ prisma/prismadb.js
@@ -1,11 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+// const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
